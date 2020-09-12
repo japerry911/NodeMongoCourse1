7b9c8a50602069f01296d6d8cdfa307fbcb66f8e
japerry911 <jack.e.perry@outlook.com>
2020-09-12 07:26:15 -0600

    basic routes and http vers in express lesson completed

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,6 +1,70 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
+
+app.use(express.json());
+
+const API_VERSION = 'v1';
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get(`/api/${API_VERSION}/tours`, (req, res) => {
+  res
+    .status(200)
+    .json({ status: 'success', results: tours.length, data: { tours: tours } });
+});
+
+app.get(`/api/${API_VERSION}/tours/:id`, (req, res) => {
+  const paramId = Number(req.params.id);
+  const tour = tours.find((tourObject) => tourObject.id === paramId);
+
+  if (!tour) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+
+  res.status(200).json({ status: 'success', data: { tour: tour } });
+});
+
+app.post(`/api/${API_VERSION}/tours`, (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({ status: 'success', data: { tour: newTour } });
+    }
+  );
+});
+
+app.patch(`/api/${API_VERSION}/tours/:id`, (req, res) => {
+  const paramId = Number(req.params.id);
+  const tour = tours.find((tourObject) => tourObject.id === paramId);
+
+  if (!tour) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+
+  res
+    .status(200)
+    .json({ status: 'success', data: { tour: '<Updated Tour Here>' } });
+});
+
+app.delete(`/api/${API_VERSION}/tours/:id`, (req, res) => {
+  const paramId = Number(req.params.id);
+  const tour = tours.find((tourObject) => tourObject.id === paramId);
+
+  if (!tour) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+
+  res.status(204).json({ status: 'success', data: null });
+});
 
 const port = 3000;
 
