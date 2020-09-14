4668f562cf2f0ae33aecd25d9017d108757b7993
japerry911 <jack.e.perry@outlook.com>
2020-09-13 19:07:15 -0600

    deleted big files

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,4 +1,48 @@
 const app = require('./app');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config({ path: `${__dirname}/config.env` });
+
+mongoose
+  .connect(
+    process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD),
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    }
+  )
+  .then((conn) => console.log('Connected to MongoDB'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997,
+});
+
+testTour
+  .save()
+  .then((doc) => console.log(doc))
+  .catch((error) => console.error(error));
 
 // START SERVER
 const port = 3000;
